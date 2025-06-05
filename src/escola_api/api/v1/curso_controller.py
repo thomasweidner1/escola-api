7e9e697d08607ac92be47edbbb2a254bda0c9127
Thomas Johannes David Weidner <thomas.jdweidner@gmail.com>
2025-06-04 21:58:29 -0300

    integração do crud com o db

--- src/escola_api/api/v1/curso_controller.py
+++ src/escola_api/api/v1/curso_controller.py
@@ -1,10 +1,10 @@
-from src.escola_api.database.banco_dados import SessionLocal
-from src.escola_api.database.modelos import CursoEntidade
-from src.escola_api.schemas.curso_schemas import Curso, CursoCadastro, CursoEditar
-from src.escola_api.app import router
 from fastapi import HTTPException, Depends
 from sqlalchemy.orm import Session
 
+from src.escola_api.app import router
+from src.escola_api.database.banco_dados import SessionLocal
+from src.escola_api.database.modelos import CursoEntidade
+from src.escola_api.schemas.curso_schemas import Curso, CursoCadastro, CursoEditar
 
 # /docs para abrir o swagger
 
@@ -14,13 +14,14 @@ cursos = [
     Curso(id=2, nome="Git e Github", sigla="GT")
 ]
 
+
 # Função de dependência para obter uma sessçao do banco de dados
 def get_db():
-    db = SessionLocal() # Cria uma nova sessão do banco de dados
+    db = SessionLocal()  # Cria uma nova sessão do banco de dados
     try:
-        yield db # Retorna a sessão de forma que o FastAPI possa utilizá-la nas rotas
+        yield db  # Retorna a sessão de forma que o FastAPI possa utilizá-la nas rotas
     finally:
-        db.close() # Garante qeue a sessão será fechada após o uso
+        db.close()  # Garante qeue a sessão será fechada após o uso
 
 
 @router.get("/api/cursos")
@@ -30,42 +31,46 @@ def listar_todos_cursos(db: Session = Depends(get_db)):
 
 
 @router.get("/api/cursos/{id}")
-def obter_por_id_curso(id: int):
-    for curso in cursos:
-        if curso.id == id:
+def obter_por_id_curso(id: int, db: Session = Depends(get_db)):
+    curso = db.query(CursoEntidade).filter(CursoEntidade.id == id).first()
+        if curso:
             return curso
     # Lançando uma exceção com o status code de 404 not found
     raise HTTPException(status_code=404, detail=f"Curso não encontrado com id: {id}")
 
 
 @router.post("/api/cursos")
-def cadastrar_curso(form: CursoCadastro):
-    ultimo_id = max([curso.id for curso in cursos], default=0)
+def cadastrar_curso(form: CursoCadastro, db: Session = Depends(get_db)):
 
     # instanciar um objeto da classe Curso
-    curso = Curso(id=ultimo_id + 1, nome=form.nome, sigla=form.sigla)
-
-    cursos.append(curso)
+    curso = CursoEntidade(nome=form.nome, sigla=form.sigla)
+    db.add(curso) # INSERT
+    db.commit() # Efetivando o registro na tabela
+    db.refresh(curso) # Preenchendo o id que foi gerado no banco de dados
 
     return curso
 
 
 @router.delete("/api/cursos/{id}")
-def apagar_curso(id: int):
-    for curso in cursos:
-        if curso.id == id:
-            cursos.remove(curso)
-            return
+def apagar_curso(id: int, db: Session = Depends(get_db)):
+    curso = db.query(CursoEntidade).filter(CursoEntidade.id == id).first()
+    if curso:
+        db.delete(curso)
+        db.commit()
+        db.refresh(curso)
+        return
 
     raise HTTPException(status_code=404, detail=f"Curso não encontrado com id: {id}")
 
 
 @router.put("/api/cursos/{id}")
-def editar_curso(id: int, form: CursoEditar):
-    for curso in cursos:
-        if curso.id == id:
-            curso.nome = form.nome
-            curso.sigla = form.sigla
-            return curso
+def editar_curso(id: int, form: CursoEditar, db: Session = Depends(get_db)):
+    curso = db.query(CursoEntidade).filter(CursoEntidade.id == id).first()
+    if curso:
+        curso.nome = form.nome
+        curso.sigla = form.sigla
+        db.commit()
+        db.refresh(curso)
+        return curso
 
     raise HTTPException(status_code=404, detail=f"Curso não encontrado com id: {id}")
